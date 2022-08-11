42113e0fdba790128faf34be61ed7cb7d75bb888
Sai Dharmendra Kanneganti <Dharma@neemanalytics.com>
2022-08-11 10:41:30 -0400

    feat: add smallestSubArrayWithGivenSum and longestSubStringLengthWithDistinctCharacters problems in sliding window

--- arrays_and_strings/sliding_window.ts
+++ arrays_and_strings/sliding_window.ts
@@ -18,3 +18,50 @@ export function maxSumSubArray(arr: number[], size: number) {
 
     return maxValue;
 }
+
+export function smallestSubArrayWithGivenSum(arr: number[], targetSum: number) {
+     if (!arr || !arr.length) return 0;
+
+     let windowStart = 0, currentWindowSum = 0, minWindowSize = Number.MAX_VALUE;
+
+     for (let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+        currentWindowSum += arr[windowEnd];
+        
+        while (currentWindowSum >= targetSum && windowStart < arr.length) {
+            minWindowSize = Math.min(minWindowSize, windowEnd - windowStart + 1);
+
+            currentWindowSum -= arr[windowStart];
+            windowStart++;
+        }
+     }
+
+     return minWindowSize;
+}
+
+export function longestSubstringLengthWithDistinctCharacters(str: string, distinctChars: number) {
+    if (!str || !str.length) return 0;
+
+    let windowStart = 0, maxSubstringLength = 0, currentRunningSum = 0, characterMap: Map<string, number> = new Map();
+
+    for (let windowEnd = 0; windowEnd < str.length; windowEnd++) {
+        const character = str[windowEnd];
+        currentRunningSum++;
+
+        if (characterMap.has(character)) {
+            let characterCount = characterMap.get(character) ?? 0;
+            characterMap.set(character, ++characterCount);
+        } else {
+            characterMap.set(character, 1);
+        }
+
+        while (characterMap.size > distinctChars) {
+            characterMap.delete(str[windowStart]);
+            windowStart++;
+            currentRunningSum--;
+        }
+        
+        maxSubstringLength = Math.max(maxSubstringLength, currentRunningSum);
+    }
+
+    return maxSubstringLength;
+}
--- main.ts
+++ main.ts
@@ -1,8 +1,12 @@
-import { maxSumSubArray } from "./arrays_and_strings/sliding_window";
+import {
+    longestSubstringLengthWithDistinctCharacters,
+    maxSumSubArray,
+    smallestSubArrayWithGivenSum,
+} from "./arrays_and_strings/sliding_window";
 
 function main() {
-   const result = maxSumSubArray([1,2,4,5,6,7,7,7,7,6,7], 4);
-   console.log(result);
+    const result = longestSubstringLengthWithDistinctCharacters("aaabbbcdeaaaaaaa", 2);
+    console.log(result);
 }
 
 main();
